750e7ad68a7b7c91cd387b4d9e5d54846c0bb22b
Dev-Iyke <iyke.thompson01@gmail.com>
2024-06-01 19:28:09 +0100

    completed the filter component

---
+++ src/components/Filter.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="filter">
+      <div class="opt">
+        <select name="location" id="location">
+          <option value="null">Location</option>
+          <option value="seaside">Seaside</option>
+          <option value="countrySide">Countryside</option>
+          <option value="village">Village</option>
+          <option value="mountains">Mountains</option>
+        </select>  
+      </div>
+      <div class="opt">
+        <select name="location" id="location">
+          <option value="miami">Miami</option>
+        </select>  
+      </div>
+      <div class="opt">
+        <select name="location" id="location">
+          <option value="miami">Miami</option>
+        </select>  
+      </div>
+      <div class="opt">
+        <select name="location" id="location">
+          <option value="miami">Miami</option>
+        </select>  
+      </div>
+      <div class="btn">
+        <button @click="filterVillas">Search</button>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Filter',
+    setup(){
+        const filterVillas = (location, dates, guests, price ) => {
+            let loc = document.getElementById('location')
+            console.log(loc.value)
+        }
+
+        return {filterVillas: filterVillas}
+    }
+
+}
+</script>
+
+<style>
+    .filter{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    border-radius: 10px;
+    padding: .6rem 0 .3rem;
+    border-bottom: 5px solid hsla(36, 22%, 73%, 1);
+    box-shadow: 0px 20px 0px 0px hsla(215, 20%, 12%, 0.72);
+    width: 60%;
+    margin: 0 auto;
+  }
+  .opt{
+    padding: 0 1rem 0 .5rem;
+    border-right: 1px solid #cbcbcb;
+  }
+  .opt select, .btn button{
+    border: none; 
+    padding: .6rem 3.5rem .6rem 0;
+    font-size: 1rem;
+    font-family: 'Courier New', Courier, monospace;
+    font-weight: 600;
+    outline: none;
+  }
+  .btn button{
+    padding: 1rem;
+    border-radius: 0 10px 10px 10px;
+    background-color: hsla(209, 10%, 39%, 1);
+    color: white;
+  }
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,21 +1,48 @@
 <template>
   <div class="hero">
     <NavBar/>
-    <h1>Luxury</h1>
+    <div class="hero-info">
+      <h1>BE OUR GUEST</h1>
+      <span>LIVE LIKE A KING IN OUR BEST HOUSES</span>
+    </div>
+    <Filter/>
   </div> 
 </template>
 
 <script>
 import NavBar from '../components/NavBar.vue'
+import Filter from '../components/Filter.vue'
 export default {
   name: "HomePage",
-    components: {NavBar},
+    components: {NavBar, Filter},
 }
 </script>
 
 <style>
   .hero{
-    height: 70vh;
-    background: url('../assets/home1.png')
+    height: 100vh;
+    background-image: url('../assets/home1.png');
+    background-blend-mode: multiply;
+    background-color: #c4c4c4;
+    background-size: cover;
+    background-repeat: no-repeat;
   }
+  .hero-info{
+    text-align: center;
+    margin: 10rem 0 6rem;
+  }
+  .hero-info h1{
+    color: white;
+    font-size: 3.2rem;
+    margin-bottom: 1rem;
+    font-family: Rufina;
+  }
+  .hero-info span{
+    background: hsla(0, 0%, 100%, 0.94);
+    padding: .7rem 1.4rem;
+    color: #C9BDAB;
+    font-weight: 500;
+    border-radius: 0 10px 10px 10px;
+  }
+  
 </style>
